4d0410f3502dc6d1c388e2c0df1d47647b804e66
marlbones <mdimaanods@gmail.com>
2017-04-17 13:27:16 +1000

    not working just yet. Temporarily displaying all products

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,7 @@ class PagesController < ApplicationController
 
   def selldash
     @products = current_user.products.order(created_at: :desc)
+    # Temporary! This isn't working how it should be. It should be just requests made specifically for users products!
+    @requests = Request.all
   end
 end
