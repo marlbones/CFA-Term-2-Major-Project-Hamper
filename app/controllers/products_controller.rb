2dd6e75e3b963a583a2da10984194ec4b936dcd4
marlbones <mdimaanods@gmail.com>
2017-04-19 09:29:31 +1000

    added markers for gmap. Altered product controller and product/index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,7 @@ class ProductsController < ApplicationController
   # GET /products.json
    # GET /index?order=[created_at, cost, contents]
   def index
+
     @products = Product.all.order(created_at: :desc)
     # Search function
     if params[:search]
@@ -21,6 +22,13 @@ class ProductsController < ApplicationController
     elsif params[:order] == 'contents'
         @products = Product.all.order('contents')
     else
+    end
+
+    @users = User.all
+    @hash = Gmaps4rails.build_markers(@users) do |user, marker|
+      marker.lat user.latitude
+      marker.lng user.longitude
+      marker.infowindow user.username
     end
   end
 
