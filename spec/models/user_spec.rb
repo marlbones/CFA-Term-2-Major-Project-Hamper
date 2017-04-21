0aadc1bc144310721908516d6a8a4a593bbf3cc1
marlbones <mdimaanods@gmail.com>
2017-04-21 20:47:00 +1000

    styled product page a bit

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,11 @@ RSpec.describe User, type: :model do
   it "user is valid" do
     user = build(:user)
     expect(user).to be_valid
+    # require 'pry'; binding.pry
+  end
+
+  it "can't be created without a password" do
+    user = build(:user, :password => nil)
+    expect(user).to_not be_valid
   end
 end
