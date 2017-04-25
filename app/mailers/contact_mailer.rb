91cbd5aae808ff4a5b64cc7dc1db20632bbb2f56
marlbones <mdimaanods@gmail.com>
2017-04-25 19:07:30 +1000

    Contact mailer refactored

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -9,6 +9,6 @@ class ContactMailer < ApplicationMailer
              subject: 'New message from website') do |format|
           format.html { render 'contact_mailer' }
           format.text { render 'contact_mailer' }
-       end
+              end
   end
 end
