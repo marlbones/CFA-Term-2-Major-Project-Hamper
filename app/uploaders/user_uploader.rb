1808ca638864ec02d7f69709519bf373595019ef
marlbones <mdimaanods@gmail.com>
2017-04-25 18:38:02 +1000

    Added fog gem and bundled. Added storage options in uploaders and keys to carrier_wave initializer

--- app/uploaders/user_uploader.rb
+++ app/uploaders/user_uploader.rb
@@ -1,11 +1,15 @@
 class UserUploader < CarrierWave::Uploader::Base
-
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -36,9 +40,9 @@ class UserUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
